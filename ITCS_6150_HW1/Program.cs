d116cddc2a08800597292e99b346a3a7cbc5d9b1
Samuel Tate <samtate123@gmail.com>
2018-09-13 22:03:16 -0400

    Add project files.

---
+++ ITCS_6150_HW1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITCS_6150_HW1 {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
